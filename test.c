fd1e2eb8695fd2a93694dcfbd3dbdc16184ab8ea
Igor V. Sementsov <ig.kuzm@gmail.com>
2025-10-29 22:06:31 +1000

    Xcode project

--- test.c
+++ test.c
@@ -2,11 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+	const char *token;
+	
 	if (argc < 2){
 		printf("usage: %s login password\n", argv[0]);
 		return 0;
 	}
-	const char *token = cyclients_get_user_token(argv[1], argv[2]);
+	
+	token = cyclients_get_user_token(argv[1], argv[2]);
 	printf("TOKEN: %s\n", token);
 	if (token == NULL) {
 		return 1;
